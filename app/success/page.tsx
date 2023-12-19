1fa6f2c6dcc202a0ba5e0b1d670b0cd4446c2bde
Malcolm Smith <malcolms65@gmail.com>
2023-12-19 23:32:55 +1100

    Success page

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const SuccessPage = () => {
-	return <div>Thank you, your inquiry has been successfully received.</div>;
+	return (
+		<div className="text-white">
+			Thank you, your inquiry has been successfully received.
+		</div>
+	);
 };
 
 export default SuccessPage;
